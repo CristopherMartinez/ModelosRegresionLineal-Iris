14d80d80283f5828b8ab73355b3d87fb9c99167f
CristopherMartinez <100528093+CristopherMartinez@users.noreply.github.com>
2023-05-02 13:33:33 -0600

    Se agregan graficas al archivo principal

--- 1_Estadistica.py
+++ 1_Estadistica.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import streamlit as st
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
 url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
 dfIris = pd.read_csv(url)
@@ -22,3 +25,31 @@ st.dataframe(dfIris.describe())
 
 st.write("Clases:")
 st.write(dfIris["variety"].value_counts())
+
+url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+dfIris = pd.read_csv(url)
+
+st.subheader("Histogramas")
+for a in range(0, len(dfIris.columns)):
+    #fig = px.histogram(go.Box(y=dfIris[a].values, name=dfIris[a]))
+    fig = px.histogram(dfIris, x=dfIris.columns[a], nbins=20, title=dfIris.columns[a])
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader("Grafica de correlacion")
+fig2 = px.scatter_matrix(dfIris, dimensions=dfIris.columns[0:4], color="variety")
+st.plotly_chart(fig2,use_container_width=True)
+
+
+st.subheader("Grafica de correlacion - Mapa de calor")
+dfIris = dfIris.drop('variety', axis=1)
+df_corr = dfIris.corr()
+fig = go.Figure()
+fig.add_trace(
+    go.Heatmap(
+        x=df_corr.columns,
+        y=df_corr.index,
+        z=np.array(df_corr)
+    )
+)
+st.plotly_chart(fig, use_container_width=True)
