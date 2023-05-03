ddf6a95eae9ff621408b45d57ddee9455c65e132
CristopherMartinez <100528093+CristopherMartinez@users.noreply.github.com>
2023-05-03 12:57:45 -0600

    Changes

--- 1_Estadistica.py
+++ 1_Estadistica.py
@@ -20,38 +20,38 @@ def analisisEstadistico():
     st.write(dfIris["variety"].value_counts())
 
 def graficas():
-    import numpy as np
-    import pandas as pd
-    import streamlit as st
-    import plotly.express as px
-    import plotly.graph_objects as go
-    import Pages 
+    #import numpy as np
+    #import pandas as pd
+    #import streamlit as st
+    #import plotly.express as px
+    #import plotly.graph_objects as go
+    #import Pages 
     
-    st.title("Graficas")
-    url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
-    dfIris = pd.read_csv(url)
+    #st.title("Graficas")
+    #url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+    #dfIris = pd.read_csv(url)
 
-    st.subheader("Histogramas")
-    for a in range(0, len(dfIris.columns)):
+    #st.subheader("Histogramas")
+    #for a in range(0, len(dfIris.columns)):
         #fig = px.histogram(go.Box(y=dfIris[a].values, name=dfIris[a]))
-        fig = px.histogram(dfIris, x=dfIris.columns[a], nbins=20, title=dfIris.columns[a])
-        st.plotly_chart(fig, use_container_width=True)
+    #    fig = px.histogram(dfIris, x=dfIris.columns[a], nbins=20, title=dfIris.columns[a])
+    #    st.plotly_chart(fig, use_container_width=True)
 
-    st.subheader("Grafica de correlacion")
-    fig2 = px.scatter_matrix(dfIris, dimensions=dfIris.columns[0:4], color="variety")
-    st.plotly_chart(fig2,use_container_width=True)
-    st.subheader("Grafica de correlacion - Mapa de calor")
-    dfIris = dfIris.drop('variety', axis=1)
-    df_corr = dfIris.corr()
-    fig = go.Figure()
-    fig.add_trace(
-        go.Heatmap(
-            x=df_corr.columns,
-            y=df_corr.index,
-            z=np.array(df_corr)
-        )
-    )
-    st.plotly_chart(fig, use_container_width=True)
+    #st.subheader("Grafica de correlacion")
+    #fig2 = px.scatter_matrix(dfIris, dimensions=dfIris.columns[0:4], color="variety")
+    #st.plotly_chart(fig2,use_container_width=True)
+    #st.subheader("Grafica de correlacion - Mapa de calor")
+    #dfIris = dfIris.drop('variety', axis=1)
+    #df_corr = dfIris.corr()
+    #fig = go.Figure()
+    #fig.add_trace(
+    #    go.Heatmap(
+    #        x=df_corr.columns,
+    #        y=df_corr.index,
+    #        z=np.array(df_corr)
+    #    )
+    #)
+    #st.plotly_chart(fig, use_container_width=True)
 
 def modelo():
     import pandas as pd
@@ -66,17 +66,10 @@ def modelo():
     x_train, x_test, y_train, y_test = train_test_split(df[df.columns[0:4]], df[df.columns[-1]], test_size=0.2)
     modelos = []
     modelo = LogisticRegression(random_state=0).fit(x_train, y_train)
-    #st.write(modelo.score(x_test, y_test))
-    #st.write(modelo.predict(x_test))
-
     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     resultados = cross_val_score(modelo, x_train, y_train,cv=kfold, scoring="accuracy")
-    #print(resultados)
-
     modeloKN = KNeighborsClassifier(n_neighbors=3)
     modeloKN.fit(x_train, y_train)
-    #st.write(modeloKN.score(x_test, y_test))
-    #st.write(modeloKN.predict(x_test))
     st.header('Modelo puntaje de Precisión:')
     st.write(modelo.score(x_test, y_test))
     st.header('Modelo Predicción:')
