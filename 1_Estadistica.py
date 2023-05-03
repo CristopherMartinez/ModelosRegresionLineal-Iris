95c8365ff3feda26e967102e4c373b620d3e4055
CristopherMartinez <100528093+CristopherMartinez@users.noreply.github.com>
2023-05-03 12:44:01 -0600

    Correciones

--- 1_Estadistica.py
+++ 1_Estadistica.py
@@ -54,21 +54,41 @@ def graficas():
 
 def modelo():
     st.title("Modelo")
-    #st.write("Puedes contactarnos en contact@example.com.")
-    # resto del código para la página "Contacto"
+    import pandas as pd
+    import streamlit as st
+    from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neighbors import KNeighborsClassifier
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    df = pd.read_csv(url, names=names)
+    x_train, x_test, y_train, y_test = train_test_split(df[df.columns[0:4]], df[df.columns[-1]], test_size=0.2)
+    modelos = []
+    modelo = LogisticRegression(random_state=0).fit(x_train, y_train)
+    #st.write(modelo.score(x_test, y_test))
+    #st.write(modelo.predict(x_test))
 
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    resultados = cross_val_score(modelo, x_train, y_train,cv=kfold, scoring="accuracy")
+    #print(resultados)
 
-# definir un diccionario para asociar cada página con su función correspondiente
+    modeloKN = KNeighborsClassifier(n_neighbors=3)
+    modeloKN.fit(x_train, y_train)
+    #st.write(modeloKN.score(x_test, y_test))
+    #st.write(modeloKN.predict(x_test))
+    st.header('Modelo puntaje de Precisión:')
+    st.write(modelo.score(x_test, y_test))
+    st.header('Modelo Predicción:')
+    st.write(modelo.predict(x_test))
+    show()
+    
+
+#Se agrego un diccionario para relacionar las paginas
 pages = {
     "Analisis Estadistico": analisisEstadistico,
     "Graficas": graficas,
     "Modelo": modelo,
 }
 
-
-# mostrar la barra lateral con las opciones de página
 selection = st.sidebar.radio("Páginas:", list(pages.keys()))
-
-
-# llamar a la función correspondiente para mostrar la página seleccionada
 pages[selection]()
